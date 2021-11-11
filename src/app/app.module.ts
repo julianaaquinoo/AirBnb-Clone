9d7dddfcb65db47c14386721476ffb0cdf0820c0
julianaaquinoo <juteixeira2012@gmail.com>
2021-11-11 15:47:45 -0300

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AcomodacaoListComponent } from './acomodacoes-list.component/acomodacoes-list.component';
-import { FiltrarComponent } from './filtrar.component/filtrar.component';
+import { FiltrarComponent } from './filtrar/filtrar.component';
+import { FiltraAcomodacoesComponent } from './filtra-acomodacoes/filtra-acomodacoes.component';
 
 
 
 @NgModule({
   imports:      [ BrowserModule,  FormsModule,RouterModule.forRoot ([{path:'',component:AcomodacaoListComponent}]) ],
-  declarations: [ AppComponent, HelloComponent, AcomodacaoListComponent, FiltrarComponent, ],
+  declarations: [ AppComponent, HelloComponent, AcomodacaoListComponent, FiltrarComponent,FiltraAcomodacoesComponent ],
   bootstrap:    [ AppComponent ], 
   
 })
---
+++ src/app/filtra-acomodacoes/filtra-acomodacoes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filtra-acomodacoes',
+  templateUrl: './filtra-acomodacoes.component.html',
+  styleUrls: ['./filtra-acomodacoes.component.css']
+})
+export class FiltraAcomodacoesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
