1ec2886b3e37ea5b3fc248d49964cdd47cd125c7
julianaaquinoo <juteixeira2012@gmail.com>
2021-11-11 16:12:37 -0300

    2 files modified

--- src/app/filtra-acomodacoes/filtra-acomodacoes.component.ts
+++ src/app/filtra-acomodacoes/filtra-acomodacoes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-filtra-acomodacoes',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FiltraAcomodacoesComponent implements OnInit {
 
-  constructor() { }
+  checkoutformlocal = this.formbuilder.group({local:'',});
+
+  constructor(private formbuilder:FormBuilder) {}
 
   ngOnInit() {
   }
